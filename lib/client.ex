a9fe6a4b82bc4e596377bc9bef3cd987947dded0
karangoel16 <karangoel16@yahoo.com>
2017-11-07 11:30:40 -0500

    added tweet to other

--- lib/client.ex
+++ lib/client.ex
@@ -6,12 +6,24 @@ defmodule Project4.Client do
     end
 
     def init(args) do
-        {:ok,%{}}
+        {:ok,{%{},%{}}} #here the first elem is for the subsriber, #second elem is for the tweets
     end
-    def handle_call(msg,_from,state) do
+    def handle_call({msg,number,tweet_msg},_from,state) do
         case msg do
-            :tweet-> IO.puts "hi I am here"
+            :tweet-> 
+                tweet=elem(state,1)
+                if Map.get(tweet,number) != nil do
+                    tweet=Map.put(tweet,number,tweet_msg)
+                    IO.inspect Enum.map(elem(state,0)|>Map.keys,fn(x)->
+                        x
+                    end)
+                end
+            :subscribe->
+                #we are adding subscribers here for the functions to work
+                map=elem(state,0)
+                map=Map.put(map,number,1)
+                state=Tuple.delete_at(state,0)|>Tuple.insert_at(0,map)
         end        
-        {:reply,state}
+    {:reply,"",state}
     end
 end
--- lib/project4.ex
+++ lib/project4.ex
@@ -13,6 +13,10 @@ defmodule Project4 do
     number_of_node=elem(args|>List.to_tuple,0)
     Project4.Exdistutils.start_distributed(:project4)
     IO.inspect Enum.map(1..String.to_integer(number_of_node),fn(x)->spawn(fn->Project4.Client.start_link(Integer.to_string(x)|>String.to_atom) end)end)
-    Process.sleep(1_000_000)
+    number_of_tweets=elem(args|>List.to_tuple,1) #This is for the number of nodes
+    #Process.sleep(1_000_000)
+    Enum.map(1..String.to_integer(number_of_tweets),fn(x)->
+      GenServer.call({:rand.uniform(String.to_integer(number_of_tweets))|>Integer.to_string|>String.to_atom,Node.self()},{:tweet,x,"hi"},:infinity)
+    end)
   end
 end
