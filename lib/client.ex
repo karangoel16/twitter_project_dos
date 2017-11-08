e12e2e9e3f3c35185ffc29dce54197d613e27665
karangoel16 <karangoel16@yahoo.com>
2017-11-08 15:57:22 -0500

    The twitter module has now MapSet for Subscriber

--- lib/client.ex
+++ lib/client.ex
@@ -2,28 +2,55 @@ defmodule Project4.Client do
     use GenServer
 
     def start_link(args) do
-        GenServer.start_link(__MODULE__,{:ok,args},name: args)
+        map=elem(GenServer.call({:Server,Node.self()},:msg),3)
+        GenServer.start_link(__MODULE__,{:ok,Map.get(map,args|>Atom.to_string|>String.to_integer,MapSet.new)},name: args)
     end
 
     def init(args) do
-        {:ok,{%{},%{}}} #here the first elem is for the subsriber, #second elem is for the tweets
+        {:ok,{args,%{},%{}}} #here the first elem is for the subsriber, #second elem is for the tweets
     end
-    def handle_call({msg,number,tweet_msg},_from,state) do
+    def handle_cast({msg,number,tweet_msg},state) do
         case msg do
             :tweet-> 
                 tweet=elem(state,1)
-                if Map.get(tweet,number) != nil do
+                if Map.get(tweet,number) == nil do
+                    map=SocialParser.extract(tweet_msg,[:hashtags,:mentions])
+                    Enum.map(Map.get(map,:hashtags,[]),fn(x)->
+                        GenServer.cast({:Server,Node.self()},{:hashtags_insert,x,number})
+                    end)
+                    Enum.map(Map.get(map,:mentions,[]),fn(x)->
+                        if GenServer.whereis({String.replace_prefix(x,"@","")|>String.to_atom,Node.self()}) != nil do
+                            GenServer.cast({String.replace_prefix(x,"@","")|>String.to_atom,Node.self()},{msg,number,tweet_msg})
+                        end
+                        GenServer.cast({:Server,Node.self()},{:mentions,x,number})
+                    end)
+                    GenServer.cast({:Server,Node.self()},{:tweets,number,tweet_msg})
                     tweet=Map.put(tweet,number,tweet_msg)
-                    IO.inspect Enum.map(elem(state,0)|>Map.keys,fn(x)->
-                        x
+                    Enum.map(elem(state,0)|>Map.keys,fn(x)->
+                        if GenServer.whereis({x|>Integer.to_string|>String.to_atom,Node.self()}) != nil do
+                            GenServer.cast({x|>Integer.to_string|>String.to_atom,Node.self()},{msg,number,tweet_msg})
+                        end
+                    end)
+                    state=Tuple.delete_at(state,1)|>Tuple.insert_at(1,tweet)
+                end
+            :retweet->
+                tweet=elem(state,1)
+                tweet_msg=Map.get(tweet,number,nil)
+                if tweet_msg != nil do
+                    Enum.map(elem(state,0)|>Map.keys,fn(x)->
+                        if GenServer.whereis({x|>Integer.to_string|>String.to_atom,Node.self()}) != nil do
+                            GenServer.cast({x|>Integer.to_string|>String.to_atom,Node.self()},{msg,number,tweet_msg})
+                        end
                     end)
                 end
+            :mention->
+                mention=elem(state,2)
             :subscribe->
                 #we are adding subscribers here for the functions to work
                 map=elem(state,0)
                 map=Map.put(map,number,1)
                 state=Tuple.delete_at(state,0)|>Tuple.insert_at(0,map)
         end        
-    {:reply,"",state}
+    {:noreply,state}
     end
 end
--- lib/project4.ex
+++ lib/project4.ex
@@ -4,19 +4,75 @@ defmodule Project4 do
   def start_link(args) do
     GenServer.start_link(__MODULE__,args,name: :Server)
   end
-  
+  '''
+    Here 0->:tweet
+         1->:hashtags
+         2->:mentions
+         3->subscriber of each system
+  '''
   def init(args) do
-    number_of_nodes=elem(args|>List.to_tuple,0)|>String.to_integer
-    {:ok,%{}}
+    {:ok,{%{},%{},%{},%{}}}
   end
+
   def main(args) do
     number_of_node=elem(args|>List.to_tuple,0)
     Project4.Exdistutils.start_distributed(:project4)
-    IO.inspect Enum.map(1..String.to_integer(number_of_node),fn(x)->spawn(fn->Project4.Client.start_link(Integer.to_string(x)|>String.to_atom) end)end)
+    Project4.start_link(args)
     number_of_tweets=elem(args|>List.to_tuple,1) #This is for the number of nodes
-    #Process.sleep(1_000_000)
-    Enum.map(1..String.to_integer(number_of_tweets),fn(x)->
-      GenServer.call({:rand.uniform(String.to_integer(number_of_tweets))|>Integer.to_string|>String.to_atom,Node.self()},{:tweet,x,"hi"},:infinity)
+    Enum.map(1..String.to_integer(number_of_node),fn(x)->
+      Enum.map(Enum.take_random(1..String.to_integer(number_of_node),5),fn(y)->
+        #IO.inspect x 
+        #IO.inspect y
+        GenServer.cast({:Server,Node.self()},{:subscribe,x,y})
+      end)
     end)
+    IO.inspect Enum.map(1..String.to_integer(number_of_node),fn(x)->spawn(fn->Project4.Client.start_link(Integer.to_string(x)|>String.to_atom) end)end)
+    Enum.map(1..String.to_integer(number_of_tweets),fn(x)->
+      GenServer.cast({:rand.uniform(String.to_integer(number_of_tweets)+1)|>Integer.to_string|>String.to_atom,Node.self()},{:tweet,x,"#hi @hi"})
+    end)
+  end
+
+  def handle_cast({msg,tweet_id,val},state) do
+     case msg do
+      :hashtags_insert->
+        hash=elem(state,1)
+        val1=Map.get(hash,tweet_id)
+        if val1==nil do
+          val1=MapSet.put(MapSet.new,val)
+        else
+          val1=MapSet.put(val1,val)
+        end
+        hash=Map.put(hash,tweet_id,val1)
+        state=Tuple.delete_at(state,1)|>Tuple.insert_at(1,hash)
+      :tweets->
+        tweet=elem(state,0)
+        tweet=Map.put(tweet,tweet_id,val)
+        IO.inspect tweet
+        state=Tuple.delete_at(state,0)|>Tuple.insert_at(0,tweet)
+      :mentions->
+        mention=elem(state,2)
+        val1=Map.get(mention,tweet_id)
+        if val1==nil do
+          val1=MapSet.put(MapSet.new,val)
+        else
+          val1=MapSet.put(val1,val)
+        end
+        mention=Map.put(mention,tweet_id,val1)
+        state=Tuple.delete_at(state,2)|>Tuple.insert_at(2,mention)
+      :subscribe->
+        #IO.puts tweet_id
+        #IO.puts val
+        subscribe=elem(state,3)
+        val1=Map.get(subscribe,tweet_id,MapSet.new([])) #this will bring out the value of tweets
+        val1=MapSet.put(val1,val)
+        subscribe=Map.put(subscribe,tweet_id,val1)
+        #IO.inspect val1
+        state=Tuple.delete_at(state,3)|>Tuple.insert_at(3,subscribe)
+     end
+     {:noreply,state}
+  end
+
+  def handle_call(msg,_from,state) do
+    {:reply,state,state}
   end
 end
