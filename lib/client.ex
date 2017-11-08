301a404464095510e787d301f89d562ad142129a
karangoel16 <karangoel16@yahoo.com>
2017-11-08 16:20:41 -0500

    correction in tweets

--- lib/client.ex
+++ lib/client.ex
@@ -3,12 +3,16 @@ defmodule Project4.Client do
 
     def start_link(args) do
         map=elem(GenServer.call({:Server,Node.self()},:msg),3)
-        GenServer.start_link(__MODULE__,{:ok,Map.get(map,args|>Atom.to_string|>String.to_integer,MapSet.new)},name: args)
+        GenServer.start_link(__MODULE__,Map.get(map,args|>Atom.to_string|>String.to_integer,MapSet.new),name: args)
     end
 
     def init(args) do
         {:ok,{args,%{},%{}}} #here the first elem is for the subsriber, #second elem is for the tweets
     end
+
+  def handle_call(msg,_from,state) do
+    {:reply,state,state}
+  end
     def handle_cast({msg,number,tweet_msg},state) do
         case msg do
             :tweet-> 
@@ -20,15 +24,15 @@ defmodule Project4.Client do
                     end)
                     Enum.map(Map.get(map,:mentions,[]),fn(x)->
                         if GenServer.whereis({String.replace_prefix(x,"@","")|>String.to_atom,Node.self()}) != nil do
-                            GenServer.cast({String.replace_prefix(x,"@","")|>String.to_atom,Node.self()},{msg,number,tweet_msg})
+                            GenServer.cast({String.replace_prefix(x,"@","")|>String.to_atom,Node.self()},{:retweet,number,tweet_msg})
                         end
                         GenServer.cast({:Server,Node.self()},{:mentions,x,number})
                     end)
                     GenServer.cast({:Server,Node.self()},{:tweets,number,tweet_msg})
                     tweet=Map.put(tweet,number,tweet_msg)
-                    Enum.map(elem(state,0)|>Map.keys,fn(x)->
+                    Enum.map(elem(state,0)|>MapSet.to_list,fn(x)->
                         if GenServer.whereis({x|>Integer.to_string|>String.to_atom,Node.self()}) != nil do
-                            GenServer.cast({x|>Integer.to_string|>String.to_atom,Node.self()},{msg,number,tweet_msg})
+                            GenServer.cast({x|>Integer.to_string|>String.to_atom,Node.self()},{:show,number,tweet_msg})
                         end
                     end)
                     state=Tuple.delete_at(state,1)|>Tuple.insert_at(1,tweet)
@@ -37,9 +41,9 @@ defmodule Project4.Client do
                 tweet=elem(state,1)
                 tweet_msg=Map.get(tweet,number,nil)
                 if tweet_msg != nil do
-                    Enum.map(elem(state,0)|>Map.keys,fn(x)->
+                    Enum.map(elem(state,0)|>MapSet.to_list,fn(x)->
                         if GenServer.whereis({x|>Integer.to_string|>String.to_atom,Node.self()}) != nil do
-                            GenServer.cast({x|>Integer.to_string|>String.to_atom,Node.self()},{msg,number,tweet_msg})
+                            GenServer.cast({x|>Integer.to_string|>String.to_atom,Node.self()},{:show,number,tweet_msg})
                         end
                     end)
                 end
@@ -50,6 +54,10 @@ defmodule Project4.Client do
                 map=elem(state,0)
                 map=Map.put(map,number,1)
                 state=Tuple.delete_at(state,0)|>Tuple.insert_at(0,map)
+            :show->
+                tweet=elem(state,1)
+                tweet=Map.put(tweet,number,tweet_msg)
+                state=Tuple.delete_at(state,1)|>Tuple.insert_at(1,tweet)
         end        
     {:noreply,state}
     end
